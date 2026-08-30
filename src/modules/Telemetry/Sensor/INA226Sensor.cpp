5c9b9572a60d5bd008ef89d508e49b5056c0563e
vadim0632 <vadim_0632@mail.ru>
2026-08-30 15:58:19 +0300

    Update INA226Sensor.cpp

--- src/modules/Telemetry/Sensor/INA226Sensor.cpp
+++ src/modules/Telemetry/Sensor/INA226Sensor.cpp
@@ -32,7 +32,7 @@ void INA226Sensor::begin(TwoWire *wire, uint8_t addr)
     _addr = addr;
     ina226 = INA226(_addr, _wire);
     _wire->begin();
-    ina226.setMaxCurrentShunt(6.0, 0.0133); // калибровочное для  шунта 
+    ina226.setMaxCurrentShunt(6.05, 0.0133); // калибровочное для  шунта 
 }
 
 bool INA226Sensor::getMetrics(meshtastic_Telemetry *measurement)
